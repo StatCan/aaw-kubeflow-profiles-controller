7a04fa1a13e74e2166c3172e305393ed7399ef71
Christian Boin <christianboin@hotmail.com>
2022-07-06 20:00:30 +0000

    feat: add edges to deployment portion

--- docs/diagrams/blobcsi_controller.py
+++ docs/diagrams/blobcsi_controller.py
@@ -17,13 +17,31 @@ def myself() -> str:
     return no_ext
 
 with Diagram(myself(), show=False):
+    tf_colour = "purple"
+    azure_colour = "blue"
+    kubernetes_colour = "darkblue"
+    github_colour = "black"
+    argocd_colour = "orange"
+    deploy_label = "deploys"
+    
+    provision_label = "provisions"
+    queries_label = "queries"
+    watches_label = "watches" 
+    allows_egress_label = "allows egress"
+    allows_ingress_label = "allows ingress"
+    auth_style = "dashed"
+    auth_label = "authenticates with"
+    
+    important_icon_width = "3"
+    important_icon_height = "3"
+
     with Cluster("aaw-argocd-manifests repo"):
         # blob-csi controller deployment
         with Cluster("/daaas-system/profile-controllers/profiles-controller"):
-            argman_profiles_controller = Custom("profiles-controller", icon_path="icons/github.png")
+            github_profiles_controller = Custom("profiles-controller", icon_path="icons/github.png")
         # blob-csi driver deployment
         with Cluster("/daaas-system/blob-csi-driver"):
-            argman_blob_csi_driver = Custom("blob-csi-driver", icon_path="icons/github.png") 
+            github_blob_csi_driver = Custom("blob-csi-driver", icon_path="icons/github.png") 
 
         # blob-csi injector
         # with Cluster("/daaas-system/blob-csi-injector"):
@@ -56,55 +74,19 @@ with Diagram(myself(), show=False):
                 tf_azure_blob_unclass_spn_secret = Custom("kubernetes_secret", icon_path="icons/terraform.png")
             with Cluster("fdi_gateway.tf"):
                 tf_opa_gateway_unclassified = Custom("fdi_minio_gateway_unclassified", icon_path="icons/terraform.png")
-                tf_opa_gateway_protb = Custom("fdi_minio_gateway_protb", icon_path="icons/terraform.png")
+                tf_opa_gateway_protected_b = Custom("fdi_minio_gateway_protb", icon_path="icons/terraform.png")
 
     with Cluster("github.com"):
         # https://github.com/StatCan/aaw-network-policies
         with Cluster("aaw-network-policies repo"):
             with Cluster("azure-blob-csi-system.yaml"):
-                allow_blob_csi_to_internet = NetworkPolicy("allow-azure-blob-csi-to-internet")
-                allow_egress_pcont_opa_protb = NetworkPolicy("allow-egress-profiles-controller-to-fdi-opa-gateway-protected-b")
-                allow_egress_pcont_opa_unclass = NetworkPolicy("allow-egress-profiles-controller-to-fdi-opa-gateway-unclassified")
-                allow_ingress_pcont_opa_protb = NetworkPolicy("allow-ingress-profiles-controller-to-fdi-opa-gateway-protected-b")
-                allow_ingress_pcont_opa_unclass = NetworkPolicy("allow-ingress-profiles-controller-to-fdi-opa-gateway-unclassified")
-
-    with Cluster("Kubernetes Cluster"):
-        # PV
-        aaw_pv_user_standard = PersistentVolume("user-standard")
-        aaw_pv_user_standard_ro = PersistentVolume("user-standard-ro")
-        aaw_pv_user_premium = PersistentVolume("user-premium")
-        aaw_pv_user_premium_ro = PersistentVolume("user-premium-ro")
-        aaw_pv_user_protected_b = PersistentVolume("user-protected-b")
-        fdi_pv_user = PersistentVolume("fdi-user-pv")
-        with Cluster("daaas-system"):
-            # profiles controller
-            profiles_controller = Pod("blob-csi.go")
-            # blob_csi_injector = Pod("blob-csi-injector")
-
-        with Cluster("user namespace"):
-            # PVC
-            aaw_pvc_user = PersistentVolumeClaim("aaw-user-pvc")
-            fdi_pvc_user = PersistentVolumeClaim("fdi-user-pvc")
-        with Cluster("fdi-gateway-unclassified-system"):
-            # minio-gateway-opa pod (opa gateway that controller queries)
-            minio_gateway_opa_unclassified = Pod("minio-gateway-opa")
-            bundle_secret_unclassified     = Secret("bundle-secret")
-        with Cluster("fdi-gateway-protected-b-system"):
-            # minio-gateway-opa pod (opa gateway that controller queries)
-            minio_gateway_opa_protb = Pod("minio-gateway-opa")
-            bundle_secret_protb     = Secret("bundle-secret")
-        with Cluster("azure-blob-csi-system"):
-            # pods
-            # blob-csi-controller
-            blob_csi_controller = Pod("blob-csi-controller")
-            # secrets
-            aaw_premium_secret = Secret("aawdevcc00samgpremium")
-            aaw_protb_secret = Secret("aawdevcc00samgprotb")
-            aaw_standard_secret = Secret("aawdevccsamgstandard")
-            azure_blob_prot_b_secret = Secret("azure-blob-csi-fdi-protected-b")
-            azure_blob_prot_b_spn_secret = Secret("azure-blob-csi-fdi-protected-b-spn")
-            azure_blob_unclass_secret = Secret("azure-blob-csi-fdi-unclassified")
-            azure_blob_unclass_spn_secret = Secret("azure-blob-csi-fdi-unclassified-spn")
+                github_allow_blob_csi_to_internet = NetworkPolicy("allow-azure-blob-csi-to-internet")
+                github_allow_egress_pcont_opa_protb = NetworkPolicy("allow-egress-profiles-controller-to-fdi-opa-gateway-protected-b")
+                github_allow_egress_pcont_opa_unclass = NetworkPolicy("allow-egress-profiles-controller-to-fdi-opa-gateway-unclassified")
+                github_allow_ingress_pcont_opa_protb = NetworkPolicy("allow-ingress-profiles-controller-to-fdi-opa-gateway-protected-b")
+                github_allow_ingress_pcont_opa_unclass = NetworkPolicy("allow-ingress-profiles-controller-to-fdi-opa-gateway-unclassified")
+            with Cluster("daaas-system.yaml"):
+                github_allow_profiles_controller_to_internet = NetworkPolicy("allow-profile-controller-to-internet")
 
     with Cluster("Azure"):
         with Cluster("FDI-DEV-RESEARCH"):
@@ -126,4 +108,164 @@ with Diagram(myself(), show=False):
             azure_standard_ro = BlobStorage("user-standard-ro")
             azure_standard = BlobStorage("user-standard")
         with Cluster("aawdevcc00samgprotb"):
-            azure_protcted_b = BlobStorage("user-protcted-b")
+            azure_protected_b = BlobStorage("user-protected-b")
+
+    with Cluster("Kubernetes Cluster"):
+        # PV
+        aaw_pv_user_standard = PersistentVolume("user-standard")
+        aaw_pv_user_standard_ro = PersistentVolume("user-standard-ro")
+        aaw_pv_user_premium = PersistentVolume("user-premium")
+        aaw_pv_user_premium_ro = PersistentVolume("user-premium-ro")
+        aaw_pv_user_protected_b = PersistentVolume("user-protected-b")
+        fdi_pv_user_unclassified = PersistentVolume("fdi-user-pv-unclassified")
+        fdi_pv_user_protected_b = PersistentVolume("fdi-user-pv-protected-b")
+
+        with Cluster("argocd-operator-system"):
+            argocd_operator = Custom("argocd-operator", icon_path="icons/argo.png")
+                # once https://github.com/mingrammer/diagrams/pull/529, bump version and implement scaled custom nodes :)
+                #height=important_icon_height, width=important_icon_width, imagescale=False)
+
+        with Cluster("daaas-system"):
+            # profiles controller
+            blobcsi_profiles_controller = Pod("blob-csi.go", height=important_icon_height,
+                width=important_icon_width, imagescale="false")
+            allow_profiles_controller_to_internet = NetworkPolicy("allow-profile-controller-to-internet")
+            # blob_csi_injector = Pod("blob-csi-injector")
+
+        with Cluster("user namespace"):
+            # PVC
+            aaw_pvc_user = PersistentVolumeClaim("aaw-user-pvc")
+            fdi_pvc_user_unclassified = PersistentVolumeClaim("fdi-user-pvc-unclassified")
+            fdi_pvc_user_protected_b = PersistentVolumeClaim("fdi-user-pvc-protected-b")
+
+        with Cluster("fdi-gateway-unclassified-system"):
+            # minio-gateway-opa pod (opa gateway that controller queries)
+            minio_gateway_opa_unclassified = Pod("minio-gateway-opa")
+            bundle_secret_unclassified     = Secret("bundle-secret")
+
+        with Cluster("fdi-gateway-protected-b-system"):
+            # minio-gateway-opa pod (opa gateway that controller queries)
+            minio_gateway_opa_protected_b = Pod("minio-gateway-opa")
+            bundle_secret_protected_b     = Secret("bundle-secret")
+
+        with Cluster("azure-blob-csi-system"):
+            # pods
+            # blob-csi-controller
+            blob_csi_controller = Pod("blob-csi-controller", height=important_icon_height,
+                width=important_icon_width, imagescale="false")
+            # secrets
+            aaw_premium_secret = Secret("aawdevcc00samgpremium")
+            aaw_protb_secret = Secret("aawdevcc00samgprotb")
+            aaw_standard_secret = Secret("aawdevccsamgstandard")
+            azure_blob_prot_b_secret = Secret("azure-blob-csi-fdi-protected-b")
+            azure_blob_prot_b_spn_secret = Secret("azure-blob-csi-fdi-protected-b-spn")
+            azure_blob_unclass_secret = Secret("azure-blob-csi-fdi-unclassified")
+            azure_blob_unclass_spn_secret = Secret("azure-blob-csi-fdi-unclassified-spn")
+
+            allow_blob_csi_to_internet = NetworkPolicy("allow-azure-blob-csi-to-internet")
+            allow_egress_pcont_opa_protb = NetworkPolicy("allow-egress-profiles-controller-to-fdi-opa-gateway-protected-b")
+            allow_egress_pcont_opa_unclass = NetworkPolicy("allow-egress-profiles-controller-to-fdi-opa-gateway-unclassified")
+            allow_ingress_pcont_opa_protb = NetworkPolicy("allow-ingress-profiles-controller-to-fdi-opa-gateway-protected-b")
+            allow_ingress_pcont_opa_unclass = NetworkPolicy("allow-ingress-profiles-controller-to-fdi-opa-gateway-unclassified")
+        
+
+
+        # ----- Deployments ----- #
+        # Terraform deploys the below secrets
+        tf_aaw_premium_secret >> Edge(color=tf_colour, label=deploy_label) >> aaw_premium_secret #
+        tf_aaw_protb_secret >> Edge(color=tf_colour, label=deploy_label) >> aaw_protb_secret 
+        tf_aaw_standard_secret >> Edge(color=tf_colour, label=deploy_label) >> aaw_standard_secret 
+        tf_azure_blob_prot_b_secret >> Edge(color=tf_colour, label=deploy_label) >> azure_blob_prot_b_secret 
+        tf_azure_blob_prot_b_spn_secret >> Edge(color=tf_colour, label=deploy_label) >> azure_blob_prot_b_spn_secret 
+        tf_azure_blob_unclass_secret >> Edge(color=tf_colour, label=deploy_label) >> azure_blob_unclass_secret 
+        tf_azure_blob_unclass_spn_secret >> Edge(color=tf_colour, label=deploy_label) >> azure_blob_unclass_spn_secret 
+
+        # Terraform deploys the unclassified and prot-b opa gateways
+        tf_opa_gateway_unclassified >> Edge(color=tf_colour, label=deploy_label) >> [minio_gateway_opa_unclassified, bundle_secret_unclassified]
+        tf_opa_gateway_protected_b >> Edge(color=tf_colour, label=deploy_label) >> [minio_gateway_opa_protected_b, bundle_secret_protected_b]
+
+        # Argocd manages the continuous deployment of the blobcsi controller
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_profiles_controller >> Edge(color=github_colour, label=deploy_label) >> blobcsi_profiles_controller
+
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_allow_blob_csi_to_internet >> Edge(color=github_colour, label=deploy_label) >> allow_blob_csi_to_internet  
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_allow_egress_pcont_opa_protb >> Edge(color=github_colour, label=deploy_label) >> allow_egress_pcont_opa_protb  
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_allow_egress_pcont_opa_unclass >> Edge(color=github_colour, label=deploy_label) >> allow_egress_pcont_opa_unclass  
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_allow_ingress_pcont_opa_protb >> Edge(color=github_colour, label=deploy_label) >> allow_ingress_pcont_opa_protb  
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_allow_ingress_pcont_opa_unclass >> Edge(color=github_colour, label=deploy_label) >> allow_ingress_pcont_opa_unclass  
+        argocd_operator >> Edge(color=argocd_colour, label="watches") >>\
+            github_allow_profiles_controller_to_internet >> Edge(color=github_colour, label=deploy_label) >> allow_profiles_controller_to_internet  
+
+        # Blobcsi controller provisions the following
+        blobcsi_profiles_controller >> Edge(color=kubernetes_colour, label=provision_label) >> [
+            # PVs
+            aaw_pv_user_standard, 
+            aaw_pv_user_standard_ro, 
+            aaw_pv_user_premium, 
+            aaw_pv_user_premium_ro, 
+            aaw_pv_user_protected_b, 
+            # PVCs
+            aaw_pvc_user, 
+        ]
+
+        # blobcsi controller creates azure containers per user for AAW
+        blobcsi_profiles_controller \
+            >> Edge(color=kubernetes_colour, style=auth_style, label=auth_label) \
+            >> aaw_premium_secret \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_profiles_controller_to_internet \
+            >> Edge(color=kubernetes_colour, label=provision_label) \
+            >> [azure_premium, azure_premium_ro]
+        blobcsi_profiles_controller \
+            >> Edge(color=kubernetes_colour, style=auth_style, label=auth_label) \
+            >> aaw_standard_secret \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_profiles_controller_to_internet \
+            >> Edge(color=kubernetes_colour, label=provision_label) \
+            >> [azure_standard, azure_standard_ro]   
+        blobcsi_profiles_controller \
+            >> Edge(color=kubernetes_colour, style=auth_style, label=auth_label) \
+            >> aaw_protb_secret \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_profiles_controller_to_internet \
+            >> Edge(color=kubernetes_colour, label=provision_label) \
+            >> azure_protected_b
+        # Blobcsi controller provisions FDI PVs after querying OPA gateways for permissions
+        blobcsi_profiles_controller  \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_egress_pcont_opa_unclass \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_ingress_pcont_opa_unclass \
+            >> Edge(color=kubernetes_colour, label=queries_label) \
+            >> minio_gateway_opa_unclassified \
+            >> Edge(color=kubernetes_colour, label=provision_label) \
+            >> [fdi_pv_user_unclassified, fdi_pvc_user_unclassified] 
+
+        blobcsi_profiles_controller \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_egress_pcont_opa_protb \
+            >> Edge(color=kubernetes_colour) \
+            >> allow_ingress_pcont_opa_protb \
+            >> Edge(color=kubernetes_colour, label=queries_label) \
+            >> minio_gateway_opa_protected_b \
+            >> Edge(color=kubernetes_colour, label=provision_label) \
+            >> [fdi_pv_user_protected_b, fdi_pvc_user_protected_b]
+    
+        # OPA Gateways supply the profiles controller by providing 
+        # http endpoints serving bundles as json responses to get requests
+        minio_gateway_opa_unclassified \
+            >> Edge(color=azure_colour, style=auth_style, label=auth_label) \
+            >> bundle_secret_unclassified \
+            << Edge(color=azure_colour) \
+            >> fdi_unclass_bundle \
+
+        # Implement logic for how blob-csi-driver mounts PV's from:
+        # 1. User creating a notebook
+        # 2. Blob-CSI driver mounts PV to azure container
+        # 3. Firewalls/networking along the way should be included, along with secrets for authentication
+        # 4. Double check everything looks good, and try to optimize diagram layout 
