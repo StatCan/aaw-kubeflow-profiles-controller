e880067d250cc2ce64d17ee8bac1efb74d4d4a6b
Jose Matsuda <jose.matsuda@statcan.gc.ca>
2024-02-26 10:37:37 -0500

    save

--- cmd/ontap-cvo.go
+++ cmd/ontap-cvo.go
@@ -3,24 +3,20 @@ package cmd
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"strings"
 	"time"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	kubeflowv1 "github.com/StatCan/profiles-controller/pkg/apis/kubeflow/v1"
 	"github.com/StatCan/profiles-controller/pkg/controllers/profiles"
 	kubeflowclientset "github.com/StatCan/profiles-controller/pkg/generated/clientset/versioned"
 	kubeflowinformers "github.com/StatCan/profiles-controller/pkg/generated/informers/externalversions"
 	"github.com/StatCan/profiles-controller/pkg/signals"
 	"github.com/spf13/cobra"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	v1 "k8s.io/client-go/listers/core/v1"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
 )
@@ -73,52 +69,13 @@ func parseSecret(name string) (string, string) {
 	return secretName, secretNamespace
 }
 
-// Create a new azblob Client using a storage account
-//
-// returns service azblob Client to the approriate storage account
-// returns err if an error is encountered
-func getBlobClient(client *kubernetes.Clientset, containerConfig AzureContainerConfig) (*azblob.Client, error) {
-
-	fmt.Println(containerConfig.SecretRef)
-	secretName, secretNamespace := parseSecret(containerConfig.SecretRef)
-	secret, err := client.CoreV1().Secrets(secretNamespace).Get(
-		context.Background(),
-		secretName,
-		metav1.GetOptions{},
-	)
-	if err != nil {
-		log.Panic(err.Error())
-		return nil, err
-	}
-
-	storageAccountName := string(secret.Data["azurestorageaccountname"])
-	storageAccountKey := string(secret.Data["azurestorageaccountkey"])
-	cred, err := azblob.NewSharedKeyCredential(storageAccountName, storageAccountKey)
-	if err != nil {
-		log.Panic(err.Error())
-		return nil, err
-	}
-
-	service, err := azblob.NewClientWithSharedKeyCredential(
-		fmt.Sprintf("https://%s.blob.core.windows.net/", storageAccountName),
-		cred,
-		nil,
-	)
-	if err != nil {
-		log.Panic(err.Error())
-		return nil, err
-	}
-
-	return service, nil
-}
-
 /*
 AIM: create a user using the API call
 This will be called with the information of the Cloud AD to create a good S3 User
 https://docs.netapp.com/us-en/ontap-restapi/ontap/protocols_s3_services_svm.uuid_users_endpoint_overview.html#creating-an-s3-user-configuration
 In addition to creating a user, this may also need to create the secret from the response given.
 */
-func createUser() {
+func createUser(ownerEmail string, namespaceStr string) {
 
 }
 
@@ -133,7 +90,7 @@ func secretExists(client *kubernetes.Clientset) bool {
 	_, err := client.CoreV1().Secrets("namespacehere").Get(context.Background(), "secretName", metav1.GetOptions{})
 	if err != nil {
 		// Then we found it? Confirm this
-		fmt.Println("Found the secret")
+		klog.Infof("Found the secret")
 		return true
 	}
 
@@ -144,7 +101,7 @@ func secretExists(client *kubernetes.Clientset) bool {
 /*
 This will if the secret has expired and then
 */
-func checkExpired() bool {
+func checkExpired(labelValue string) bool {
 	// If expired
 	return true
 	// Not found
@@ -177,6 +134,7 @@ var ontapcvoCmd = &cobra.Command{
 		}
 
 		// Setup informers
+		// kubeflow informer is necessary for watching profile updates
 		kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Minute*(time.Duration(requeue_time)))
 		kubeflowInformerFactory := kubeflowinformers.NewSharedInformerFactory(kubeflowClient, time.Minute*(time.Duration(requeue_time)))
 
@@ -197,11 +155,13 @@ var ontapcvoCmd = &cobra.Command{
 					if k == ontapLabel {
 						if !secretExists(kubeClient) {
 							// if the secret does not exist, then do API call to create user
-							createUser()
+							klog.Infof("Secret for " + profile.Name + " not found. Creating User")
+							createUser(profile.Spec.Owner.Name, profile.Name)
 						}
 						// Secret does exist do nothing, or for future iteration can check for expiration date
-						if checkExpired() {
+						if checkExpired(v) {
 							// Do things, but for first iteration may not care.
+							klog.Infof("Expired, but not going to do anything yet")
 						}
 					}
 				} // End iterating through labels on profile
@@ -210,28 +170,8 @@ var ontapcvoCmd = &cobra.Command{
 			}, // end controller setup
 		)
 
-		configMapInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-			UpdateFunc: func(old, new interface{}) {
-				newDep := new.(*corev1.ConfigMap)
-				oldDep := old.(*corev1.ConfigMap)
-
-				if newDep.ResourceVersion == oldDep.ResourceVersion {
-					return
-				}
-
-				controller.HandleObject(new)
-			},
-			DeleteFunc: controller.HandleObject,
-		})
-
 		kubeInformerFactory.Start(stopCh)
 		kubeflowInformerFactory.Start(stopCh)
-
-		// Wait for caches to sync
-		klog.Info("Waiting for configMap informer caches to sync")
-		if ok := cache.WaitForCacheSync(stopCh, configMapInformer.Informer().HasSynced); !ok {
-			klog.Fatalf("failed to wait for caches to sync")
-		}
 
 		// Run the controller
 		if err = controller.Run(2, stopCh); err != nil {
@@ -241,5 +181,5 @@ var ontapcvoCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.AddCommand(blobcsiCmd)
+	rootCmd.AddCommand(ontapcvoCmd)
 }
