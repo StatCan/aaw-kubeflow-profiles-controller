910c31b3d5791a53a4399dfa32e5da214daa40a2
Jose Manuel (Ito) <jose.matsuda@statcan.gc.ca>
2024-03-07 13:14:20 -0500

    ontap-cvo user controller beginnings (#141)
    * feat(netapp user controller): first iteration
    
    ---------
    
    Co-authored-by: Mathis Marcotte <mathis.marcotte@statcan.gc.ca>

---
+++ cmd/ontap-cvo.go
@@ -0,0 +1,305 @@
+package cmd
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	azidentity "github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	kubeflowv1 "github.com/StatCan/profiles-controller/pkg/apis/kubeflow/v1"
+	"github.com/StatCan/profiles-controller/pkg/controllers/profiles"
+	kubeflowclientset "github.com/StatCan/profiles-controller/pkg/generated/clientset/versioned"
+	kubeflowinformers "github.com/StatCan/profiles-controller/pkg/generated/informers/externalversions"
+	"github.com/StatCan/profiles-controller/pkg/signals"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	"github.com/microsoftgraph/msgraph-sdk-go/users"
+	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+)
+
+/** Implementation Notes
+Currently strongly based on the blob-csi
+We will look for a label on the profile, and if it exists we will do the account creation via api call.
+That is step 1, eventually may also want the mounting to happen here but scoping to just account creation.
+
+For mounting there are a lot of helpful useful functions in `blob-csi.go` that we can re-use
+  like the building of the pv / pvc spec, the creation and deletion of them etc.
+*/
+
+const ontapLabel = "ontap-cvo"
+
+// const automountLabel = "blob.aaw.statcan.gc.ca/automount"
+type s3keys struct { // i doubt this works
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+}
+
+type createUserResponse struct {
+	numRecords int         `json:"num_records"`
+	records    []s3KeysObj `json:"records"`
+}
+
+type s3KeysObj struct {
+	name      string `json:"name"`
+	AccessKey string `json:"access_key"`
+	SecretKey string `json:"secret_key"`
+}
+
+/*
+Send a request to the NetApp API to create an S3 User and store the result in a k8s secret
+Uses the onPremName, the namespace, and filer to create and specify the secret.
+*/
+func createUser(onPremName string, namespaceStr string, client *kubernetes.Clientset, filerStr string) bool {
+	//Encode the data
+	postBody, _ := json.Marshal(map[string]string{
+		"name": onPremName,
+	})
+	//Leverage Go's HTTP Post function to make request
+
+	// Retrieve cm containting SVM uuids and IPs
+	netappCm, err := client.CoreV1().ConfigMaps("netapp").Get(context.Background(), "netapp-filer-connection-info", metav1.GetOptions{})
+	if err != nil {
+		klog.Infof("An Error Occured while getting netapp filer connection info %v", err)
+		return false
+	}
+	filerUUID := netappCm.Data[filerStr+"svm"]
+	// ^ this will fail if it doesnt follow the filerStr+svm, which is the case for some SAS filers.
+	// Retrieve the management IP and user login information to authenticate with netapp API
+	secret, err := client.CoreV1().Secrets("netapp").Get(context.Background(), "netapp-management-information", metav1.GetOptions{})
+	if err != nil {
+		klog.Infof("An Error Occured while getting netapp management information secret %v", err)
+		return false
+	}
+
+	managementIP := string(secret.Data["management_ip"])
+	netappUser := string(secret.Data[filerStr+"_user"])
+	// netappUserAccess := string(secret.Data[filerStr+"_access"])
+	netappUserSecret := string(secret.Data[filerStr+"_secret"])
+
+	url := "https://" + managementIP + "/api/protocols/s3/services/" + filerUUID + "/users"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(postBody))
+	req.Header.Set("Content-Type", "application/json")
+	// req.Header. // need to set other information
+	authorization := base64.StdEncoding.EncodeToString([]byte(netappUser + ":" + netappUserSecret)) // this must be confirmed)
+	req.Header.Set("Authorization", "Basic "+authorization)
+	// resp, err := http.Post(url, "application/json", bytes.NewBuffer(postBody)) // cant use this because http.post does not allow for additional headers
+	//Handle Error
+	/*if err != nil {
+		klog.Infof("An Error Occured while calling s3 services %v", err)
+	}*/
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		klog.Fatalf("error sending and returning HTTP response  : %v", err)
+	}
+	// defer resp.Body.Close() // unsure if needed?
+
+	//post := &s3keys{}
+	post := &createUserResponse{}
+	derr := json.NewDecoder(resp.Body).Decode(post)
+	if derr != nil {
+		panic(derr)
+	}
+	if resp.StatusCode != http.StatusCreated {
+		panic(resp.Status)
+	}
+	// Now that we have the values for the keys put it into a secret in the namespace
+	usersecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      filerStr + "-conn-secret", // change to be a const later or something
+			Namespace: namespaceStr,
+		},
+		Data: map[string][]byte{
+			// this [0] seems a bit suspect but we will see how it works for now I don't know
+			// I don't think the S3 account will be used multiple times or anything
+			"S3_ACCESS": []byte(post.records[0].AccessKey),
+			"S3_SECRET": []byte(post.records[0].SecretKey),
+		},
+	}
+
+	_, err = client.CoreV1().Secrets(namespaceStr).Create(context.Background(), usersecret, metav1.CreateOptions{})
+	if err != nil {
+		klog.Infof("An Error Occured while creating the secret %v", err)
+		return false
+	}
+
+	return true
+}
+
+/*
+This will get the onPremName given the owner email
+*/
+func getOnPrem(ownerEmail string, client *kubernetes.Clientset) (string, bool) {
+	klog.Infof("Retrieving onpremisis Name")
+	// Step 0 Get the App Registration Info
+	// Don't forget to create a secret in the namespace for authentication with azure in the namespace
+	// for me in aaw-dev its under jose-matsuda
+	secret, err := client.CoreV1().Secrets("netapp").Get(context.Background(), "netapp-regi-secret", metav1.GetOptions{})
+	if err != nil {
+		klog.Infof("An Error Occured while getting registration secret %v", err)
+		return "", false
+	}
+
+	TENANT_ID := string(secret.Data["TENANT_ID"])
+	CLIENT_ID := string(secret.Data["CLIENT_ID"])
+	CLIENT_SECRET := string(secret.Data["CLIENT_SECRET"])
+
+	// Step 1 is authenticating with Azure to get the `onPremisesSamAccountName` to be used as an S3 user
+	cred, err := azidentity.NewClientSecretCredential(
+		TENANT_ID,
+		CLIENT_ID,
+		CLIENT_SECRET,
+		nil,
+	)
+	if err != nil {
+		klog.Infof("client credential error: %v", err)
+		return "", false
+	}
+
+	graphClient, err := msgraphsdk.NewGraphServiceClientWithCredentials(
+		cred, []string{"https://graph.microsoft.com/.default"})
+	if err != nil {
+		klog.Infof("graph client error: %v", err)
+		return "", false
+	}
+
+	query := users.UserItemRequestBuilderGetQueryParameters{
+		Select: []string{"onPremisesSamAccountName"},
+	}
+
+	options := users.UserItemRequestBuilderGetRequestConfiguration{
+		QueryParameters: &query,
+	}
+
+	result, err := graphClient.Users().ByUserId(ownerEmail).Get(context.Background(), &options)
+	if err != nil {
+		klog.Infof("An Error Occured while trying to retrieve on prem name: %v", err)
+		return "", false
+	}
+
+	onPremAccountName := result.GetOnPremisesSamAccountName()
+	if onPremAccountName == nil {
+		klog.Infof("No on prem name found for user: %s", ownerEmail)
+		return "", false
+	}
+	return *onPremAccountName, true
+}
+
+/*
+Using the profile namespace, will use the configmap to retrieve a list of filers attached to the profile
+It will then iterate over the list and search for a constructed secret and if that secret is not found then we create
+the S3 user (and as a result the secret)
+*/
+func checkSecrets(client *kubernetes.Clientset, profileName string, profileEmail string) bool {
+	// We don't actually need secret informers, since informers look at changes in state
+	// https://www.macias.info/entry/202109081800_k8s_informers.md
+	// Get a list of secrets the user namespace should have accounts for using the configmap
+	klog.Infof("Searching for secrets for " + profileName)
+	filers, _ := client.CoreV1().ConfigMaps(profileName).Get(context.Background(), "user-filers-cm", metav1.GetOptions{})
+	for k, _ := range filers.Data {
+		// have to iterate and check secrets
+		klog.Infof("Searching for: " + k + "-conn-secret")
+		_, err := client.CoreV1().Secrets(profileName).Get(context.Background(), k+"-conn-secret", metav1.GetOptions{})
+		if err != nil {
+			klog.Infof("Error found, possbily secret not found, creating secret")
+			// Get the OnPremName
+			onPremName, foundOnPrem := getOnPrem(profileEmail, client)
+			if foundOnPrem {
+				// Create the user
+				wasSuccessful := createUser(onPremName, profileName, client, k)
+				if !wasSuccessful {
+					klog.Info("Unable to create S3 user")
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+/*
+This will if the secret has expired and then
+*/
+func checkExpired(labelValue string) bool {
+	// If expired
+	return true
+	// Not found
+	return false
+}
+
+var ontapcvoCmd = &cobra.Command{
+	Use:   "ontap-cvo",
+	Short: "Configure ontap-cvo credentials",
+	Long:  `Configure ontap-cvo credentials`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Setup signals so we can shutdown cleanly
+		stopCh := signals.SetupSignalHandler()
+
+		// Create Kubernetes config
+		cfg, err := clientcmd.BuildConfigFromFlags(apiserver, kubeconfig)
+		if err != nil {
+			klog.Fatalf("error building kubeconfig: %v", err)
+		}
+
+		// Builds k8s client for us to use, pass this in to functions for us to use
+		kubeClient, err := kubernetes.NewForConfig(cfg)
+		if err != nil {
+			klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+		}
+
+		kubeflowClient, err := kubeflowclientset.NewForConfig(cfg)
+		if err != nil {
+			klog.Fatalf("error building Kubeflow client: %v", err)
+		}
+
+		// Setup informers
+		// kubeflow informer is necessary for watching profile updates
+		kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Minute*(time.Duration(requeue_time)))
+		kubeflowInformerFactory := kubeflowinformers.NewSharedInformerFactory(kubeflowClient, time.Minute*(time.Duration(requeue_time)))
+
+		// Retrieve Information from configmaps
+		// I don't think I need informers, im not watching for updates, this thing watches on profiles anyways and can just
+		// get the information when I need it
+		//configMapInformer := kubeInformerFactory.Core().V1().ConfigMaps()
+		//configMapLister := configMapInformer.Lister()
+
+		// Setup controller
+		controller := profiles.NewController(
+			kubeflowInformerFactory.Kubeflow().V1().Profiles(),
+			func(profile *kubeflowv1.Profile) error {
+				allLabels := profile.Labels
+				for k, v := range allLabels {
+					// If the label we specify exists then look for the secret
+					if k == ontapLabel {
+						checkSecrets(kubeClient, profile.Name, profile.Spec.Owner.Name)
+						if checkExpired(v) {
+							// Do things, but for first iteration may not care.
+							//klog.Infof("Expired, but not going to do anything yet")
+						}
+					}
+				} // End iterating through labels on profile
+				// Could also check for deleting S3 users + secret clean up but maybe not for first iteration.
+				return nil
+			}, // end controller setup
+		)
+
+		kubeInformerFactory.Start(stopCh)
+		kubeflowInformerFactory.Start(stopCh)
+
+		// Run the controller
+		if err = controller.Run(2, stopCh); err != nil {
+			klog.Fatalf("error running controller: %v", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(ontapcvoCmd)
+}
--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ module github.com/StatCan/profiles-controller
 go 1.19
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.5.1
 	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.1.0
 	github.com/argoproj/argo-cd/v2 v2.8.4
 	github.com/lib/pq v1.10.5
+	github.com/microsoftgraph/msgraph-sdk-go v1.34.0
 	github.com/spf13/cobra v1.7.0
 	istio.io/api v0.0.0-20220127171628-dd6bd11b8b31
 	istio.io/client-go v1.12.2
@@ -21,8 +23,8 @@ require (
 require (
 	cloud.google.com/go/compute v1.19.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.6.0 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/internal v1.3.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.2 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.2 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
 	github.com/Azure/go-autorest/autorest v0.11.27 // indirect
@@ -30,6 +32,7 @@ require (
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.1 // indirect
 	github.com/MakeNowJust/heredoc v0.0.0-20170808103936-bb23615498cd // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/Microsoft/go-winio v0.5.2 // indirect
@@ -42,6 +45,7 @@ require (
 	github.com/bradleyfalzon/ghinstallation/v2 v2.5.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chai2010/gettext-go v0.0.0-20170215093142-bf70f2a70fb1 // indirect
+	github.com/cjlapao/common-go v0.0.39 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
@@ -56,7 +60,8 @@ require (
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.4.1 // indirect
 	github.com/go-git/go-git/v5 v5.7.0 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
@@ -64,16 +69,17 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-github/v53 v53.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -84,8 +90,17 @@ require (
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/microsoft/kiota-abstractions-go v1.5.6 // indirect
+	github.com/microsoft/kiota-authentication-azure-go v1.0.2 // indirect
+	github.com/microsoft/kiota-http-go v1.3.0 // indirect
+	github.com/microsoft/kiota-serialization-form-go v1.0.0 // indirect
+	github.com/microsoft/kiota-serialization-json-go v1.0.6 // indirect
+	github.com/microsoft/kiota-serialization-multipart-go v1.0.0 // indirect
+	github.com/microsoft/kiota-serialization-text-go v1.0.0 // indirect
+	github.com/microsoftgraph/msgraph-sdk-go-core v1.0.2 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
 	github.com/moby/spdystream v0.2.0 // indirect
 	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
@@ -98,7 +113,9 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/redis/go-redis/v9 v9.0.5 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/russross/blackfriday v1.6.0 // indirect
@@ -106,20 +123,25 @@ require (
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/skeema/knownhosts v1.1.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/std-uritemplate/std-uritemplate/go v0.0.50 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/vmihailenco/go-tinylfu v0.2.2 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/xlab/treeprint v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.22.0 // indirect
+	go.opentelemetry.io/otel/metric v1.22.0 // indirect
+	go.opentelemetry.io/otel/trace v1.22.0 // indirect
 	go.starlark.net v0.0.0-20220328144851-d1966c6b9fcd // indirect
-	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/oauth2 v0.9.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
